7ac50d777b7bf04b52dbc28def4a76236fa422f9
Andreas Liljeqvist <bonega@gmail.com>
2011-08-02 13:07:49 +0200

    Bumped version to keep in sync with clojars.
    Signed-off-by: Andreas Liljeqvist <bonega@gmail.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pacl "0.1.0-SNAPSHOT"
+(defproject pacl "0.1.1-SNAPSHOT"
   :description "Library for more sane handling of archives."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
