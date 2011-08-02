bcef2051f7367d149edbfb1417ccce0e35091378
Andreas Liljeqvist <bonega@gmail.com>
2011-08-03 00:57:39 +0200

    Updated dependencies to make Rar work.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject pacl "0.1.1-SNAPSHOT"
+(defproject pacl "0.1.2-SNAPSHOT"
   :description "Library for more sane handling of archives."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojars.bonega/java-unrar "0.5.0"]]
+                 [org.clojars.bonega/java-unrar "0.5.0"]
+                 [commons-logging "1.0.4"]]
   :dev-dependencies [[swank-clojure "1.3.1"]])
